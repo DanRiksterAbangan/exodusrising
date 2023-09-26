f5a0b6d2e471cd240af547faa246d3f535032b8b
Reymond <reymondsiarot@gmail.com>
2023-09-26 23:11:59 +0800

    added laravel echo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 import './bootstrap';
+
+
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+import io from "socket.io-client";
+window.io = io;
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: "http://172.30.202.124/",
+
+});
+window.Echo.connector.socket.on("connect", function() {
+    console.debug("Connected");
+});
+window.Echo.connector.socket.on("disconnect", function() {
+    console.debug("disconnected");
+});
+window.Echo.connector.socket.on("error", function() {
+console.debug("error");
+});
+
+
