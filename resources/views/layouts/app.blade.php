44a4529bebb4614a50f4e6c624c169babba13483
Reymond <reymondsiarot@gmail.com>
2023-10-02 18:46:24 +0800

    modify footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,8 +32,9 @@
                             <div
                                 class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
                                 <div class="text-dark order-2 order-md-1">
-                                    <span class="text-muted fw-semibold me-1">2023&copy;</span>
-                                    <a href="/" target="_blank" class="text-gray-800 text-hover-primary">Rohan</a>
+                                    <span class="text-muted fw-semibold me-1">{{ Carbon::now()->format('Y') }}&copy;</span>
+                                    <a href="/" target="_blank" class="text-gray-800 text-hover-primary">Global Rohan
+                                    </a>
                                 </div>
 
                             </div>
