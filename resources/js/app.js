aad1251ba577e6c88b03abcfcc66e5e20033a62f
Reymond <reymondsiarot@gmail.com>
2023-11-01 18:16:34 +0800

    remove socket io

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,26 @@
 import './bootstrap';
 
 
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
-import io from "socket.io-client";
+
 import Tagify from '@yaireo/tagify'
 
 window.Tagify = Tagify;
-window.io = io;
-window.Pusher = Pusher;
+// window.io = io;
+// window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'socket.io',
-    host: "http://172.30.202.124/",
+// window.Echo = new Echo({
+//     broadcaster: 'socket.io',
+//     host: "http://172.30.202.124/",
 
-});
-window.Echo.connector.socket.on("connect", function() {
-    console.debug("Connected");
-});
-window.Echo.connector.socket.on("disconnect", function() {
-    console.debug("disconnected");
-});
-window.Echo.connector.socket.on("error", function() {
-console.debug("error");
-});
+// });
+// window.Echo.connector.socket.on("connect", function() {
+//     console.debug("Connected");
+// });
+// window.Echo.connector.socket.on("disconnect", function() {
+//     console.debug("disconnected");
+// });
+// window.Echo.connector.socket.on("error", function() {
+// console.debug("error");
+// });
 
 
