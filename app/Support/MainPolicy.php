9a39bcfa71bdc0a2b0a97e6c1c7e78ed496d787b
Reymond <reymondsiarot@gmail.com>
2023-11-04 20:44:27 +0800

    fix

--- app/Support/MainPolicy.php
+++ app/Support/MainPolicy.php
@@ -11,7 +11,9 @@ class MainPolicy extends Basic
     public function configure()
     {
         parent::configure();
-        $this->addDirective(Directive::DEFAULT,'*.googleapis.com');
+        $this->addDirective(Directive::STYLE,'self  fonts.googleapis.com');
+        $this->addDirective(Directive::FONT,'self data:');
+        $this->addDirective(Directive::FONT,'self fonts.gstatic.com');
         if (app()->environment('local')) {
             $this->addDirective(Directive::SCRIPT, '127.0.0.1:5173');
             $this->addDirective(Directive::STYLE, '127.0.0.1:5173');
