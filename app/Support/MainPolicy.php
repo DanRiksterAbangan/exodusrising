a2992b46853ea09480b5798a0746eb63cac522c5
Reymond <reymondsiarot@gmail.com>
2023-11-06 14:39:11 +0800

    update policy

--- app/Support/MainPolicy.php
+++ app/Support/MainPolicy.php
@@ -12,16 +12,15 @@ class MainPolicy extends Basic
     {
         parent::configure();
         $this->addDirective(Directive::STYLE, ' fonts.googleapis.com')
-            ->addDirective(Directive::STYLE, ' fonts.gstatic.com')
-            ->addDirective(Directive::FONT, ' fonts.gstatic.com/s/')
-            ->addDirective(Directive::FONT, ' data:');
+        ->addDirective(Directive::STYLE, ' fonts.gstatic.com')
+        ->addDirective(Directive::FONT, ' fonts.gstatic.com/s/')
+        ->addDirective(Directive::FONT, ' data:')
+        ->addDirective(Directive::FONT, 'self')
+        ->addDirective(Directive::SCRIPT,'unsafe-eval unsafe-inline');
         if (app()->environment('local')) {
             $this->addDirective(Directive::SCRIPT, '127.0.0.1:5173');
             $this->addDirective(Directive::STYLE, '127.0.0.1:5173')
-            ->addDirective(Directive::FONT, 'self')
-            ->addDirective(Directive::SCRIPT,['http://gc.kis.v2.scr.kaspersky-labs.com']);
+            ->addDirective(Directive::FONT, 'self');
         }
-
-
     }
 }
